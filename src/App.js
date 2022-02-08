2dd54f72a7ec6428b72fd7a6439640d61668cd87
VitaPavlenko <91799017+VitaPavlenko@users.noreply.github.com>
2022-02-08 16:55:22 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import "./App.css";
+import ContactForm from "./Components/ContactForm/ContactForm";
+import Filter from "./Components/Filter/Filter";
+import ContactList from "./Components/ContactList/ContactList";
+import React, { Component } from "react";
+import { nanoid } from "nanoid";
+class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+  };
+
+  addContact = (newContact) => {
+    const searchContactByName = this.state.contacts
+      .map((contact) => contact.name)
+      .includes(newContact.name);
+
+    if (searchContactByName) {
+      alert(`${newContact.name} is already in contacts.`);
+      return;
+    }
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  handleFilter = (event) => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  deletaClick = (nameId) => {
+    console.log("ok");
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== nameId),
+    }));
+  };
+
+  render() {
+    const { contacts, name, number, filter } = this.state;
+
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <ContactForm addContact={this.addContact} />
+        <h2>Contacts</h2>
+        <Filter filter={this.state.filter} handleFilter={this.handleFilter} />
+        <ContactList
+          contacts={this.state.contacts}
+          filter={this.state.filter}
+          deletaClick={this.deletaClick}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
